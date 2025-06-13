05fab6188b8a19ffbd925a5045fe62824cde1f1d
giorgoschionas <giorgosch8@gmail.com>
2025-06-13 01:21:35 +0100

    Added functions in AMMEnvironment: step, normalise_observation, normalise_action, normalise_rewards

--- SAiFE_gym/gym/AMMEnvironment.py
+++ SAiFE_gym/gym/AMMEnvironment.py
@@ -9,6 +9,7 @@ from SAiFE_gym.stochastic_processes.price_impact_models import PriceImpactModel
 from SAiFE_gym.gym.ModelDynamics import ModelDynamics
 from SAiFE_gym.agents.Agent import Agent
 from SAiFE_gym.rewards import RewardFunction
+from SAiFE_gym.gym.index_names import TIME_INDEX, NUM_ARRIVALS_INDEX, NUM_FILLS_INDEX, SPOT_PRICE_INDEX, RESERVES_INDEX
 
 
 
@@ -49,8 +50,45 @@ class AMMEnvironment(gym.Env):
         for i, process in enumerate(self.stochastic_processes.values()):
             process.seed(seed + i + 1)
 
+    # The step function is a core component of reinforcement learning environments, simulating one discrete time step 
+    # within the trading environment. It takes an action from the agent, updates the environment's state based on that action, 
+    # calculates the resulting reward, and determines if the episode has concluded.
+    # It is standard structure for environments in OpenAI's Gym.
     def step(self, action: np.ndarray):
+        action = self.normalise_action(action, inverse=True)
+        current_state = self.model_dynamics.state.copy()
+        next_state = self._update_state(action)
+        dones = self._get_dones()
+        rewards = self.reward_function.calculate(current_state, action, next_state, dones[0])
+        infos = self._calculate_infos(current_state, action, rewards)
+        return self.normalise_observation(next_state.copy()), self.normalise_rewards(rewards), dones, infos
+
+    def normalise_observation(self, obs: np.ndarray, inverse: bool = False):
+        if self.normalise_observation_space_ and not inverse:
+            return (obs - self._intercept_obs_norm) / self._gradient_obs_norm - 1
+        elif self.normalise_observation_space_ and inverse:
+            return (obs + 1) * self._gradient_obs_norm + self._intercept_obs_norm
+        else:
+            return obs
+
+    def normalise_action(self, action: np.ndarray, inverse: bool = False):
+        if self.normalise_action_space_ and not inverse:
+            return (action - self._intercept_action_norm) / self._gradient_action_norm - 1
+        elif self.normalise_action_space_ and inverse:
+            return (action + 1) * self._gradient_action_norm + self._intercept_action_norm
+        else:
+            return action
+    
+    def normalise_rewards(self, rewards: np.ndarray):
+        return self.reward_scaling * rewards if self.normalise_rewards_ else rewards
+
+
+    # should be something like state[0]=reserves of asset 0, state[1]=reserves of asset 1, state[2]=spot price of AMM
+    # and then remaining states depend on
+    # the dimensionality of the arrival process, the midprice process
+    def _udpate_state(self, actions: np.ndarray):
         pass
+        
 
     def _update_market_state(self, arrivals: np.ndarray, fills: np.ndarray, actions: np.ndarray):
         pass
@@ -59,4 +97,6 @@ class AMMEnvironment(gym.Env):
         pass
 
     
-
+    def _get_dones(self):
+        done = self.model_dynamics.state[0, TIME_INDEX] >= self.terminal_time - self.step_size / 2
+        return np.full((self.num_trajectories,), done, dtype=bool)
